fe5b824debd8ad99dfa68c3d3ae21b66bf91dc92
Aleksandr Maksimov <sm-1@inbox.lv>
2021-02-07 15:38:10 +0200

    Initial commit

---
+++ ex_01.py
@@ -0,0 +1,23 @@
+class Matrix():
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        return '\n'.join(['  '.join(['%d' % i for i in row]) for row in self.matrix])
+
+    def __add__(self, other):
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix)):
+                self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+        return Matrix(self.matrix)
+
+
+a = Matrix([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]])
+print(a)
+print(' +')
+b = Matrix([[2, 4, 6, 8], [4, 6, 8, 10], [6, 8, 10, 12], [8, 10, 12, 14]])
+print(b)
+print(' =')
+m = a + b
+print(m)
+
